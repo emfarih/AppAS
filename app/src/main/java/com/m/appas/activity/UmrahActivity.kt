7878f295c644ecbd0be7a1dc2d53eb5c1417b89d
EM Farih <em.farih@gmail.com>
2019-12-01 17:26:24 +0700

    Create Dummy Activity

---
+++ app/src/main/java/com/m/appas/activity/UmrahActivity.kt
@@ -0,0 +1,13 @@
+package com.m.appas.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.m.appas.R
+
+class UmrahActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_umrah)
+    }
+}
