d77a6a0e374782d4076a6f3fea7ba92d7ba03e1e
EM Farih <em.farih@gmail.com>
2019-12-02 19:15:01 +0700

    Try to add Pay Invoice Action.

--- app/src/main/java/ml/farih/appas/ui/provider/MainSliceProvider.kt
+++ app/src/main/java/ml/farih/appas/ui/provider/MainSliceProvider.kt
@@ -7,22 +7,22 @@ import android.content.Intent
 import android.net.Uri
 import android.util.Log
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.IconCompat
+import androidx.core.graphics.drawable.IconCompat.createWithBitmap
 import androidx.core.graphics.drawable.IconCompat.createWithResource
 import androidx.slice.Slice
 import androidx.slice.SliceProvider
 import androidx.slice.builders.*
 import androidx.slice.builders.ListBuilder.*
 import com.bumptech.glide.Glide
-import ml.farih.appas.data.ApiResponse
-import ml.farih.appas.data.Repository
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import ml.farih.appas.R
+import ml.farih.appas.data.ApiResponse
+import ml.farih.appas.data.Repository
 import ml.farih.appas.di.DaggerAppComponent
 import ml.farih.appas.ui.activity.MainActivity
-import java.lang.Exception
 import javax.inject.Inject
+import ml.farih.appas.R
+
 
 @SuppressLint("Slices")
 class MainSliceProvider : SliceProvider() {
@@ -30,7 +30,7 @@ class MainSliceProvider : SliceProvider() {
 
     @Inject
     lateinit var repository: Repository
-    var data = listOf(ApiResponse(), ApiResponse())
+    var data = listOf(ApiResponse(), ApiResponse(), ApiResponse())
 
 
     override fun onCreateSliceProvider(): Boolean {
@@ -43,15 +43,34 @@ class MainSliceProvider : SliceProvider() {
         when (sliceUri.path) {
             "/account" -> return createAccountSlice(sliceUri)
             "/order" -> return createOrderSlice(sliceUri)
+            "/pay_invoice" -> return createPayInvoiceSlice(sliceUri)
         }
         return null
+    }
+
+    private fun createPayInvoiceSlice(sliceUri: Uri): Slice? {
+        val futureTarget = Glide.with(contextNonNull)
+            .asBitmap()
+            .load(data[2].imageUri)
+            .submit()
+        val bitmap = futureTarget.get()
+
+
+        if (data[2].name == "Loading...") getDataFromNetwork(sliceUri)
+        return list(contextNonNull, sliceUri, INFINITY) {
+            setAccentColor(0xff4285) // Specify color for tinting icons / controls.
+            row {
+                title = "Wi-Fi"
+                addEndItem(createWithBitmap(bitmap), SMALL_IMAGE)
+            }
+        }
     }
 
     private fun createOrderSlice(sliceUri: Uri): Slice? {
         val mainPendingIntent = PendingIntent.getActivity(
             contextNonNull,
             sliceUri.hashCode(),
-            Intent(contextNonNull,MainActivity::class.java),
+            Intent(contextNonNull, MainActivity::class.java),
             0
         )
 
@@ -75,7 +94,7 @@ class MainSliceProvider : SliceProvider() {
                         .load("https://www.batamnews.co.id/foto_berita/83kuda.jpg")
                         .submit()
                     val bitmap = futureTarget.get()
-                    addImage(IconCompat.createWithBitmap(bitmap), LARGE_IMAGE)
+                    addImage(createWithBitmap(bitmap), LARGE_IMAGE)
                 }
             }
         }
